0d08f026fcfefc61d6eb823b396e2104f6b0bf81
Miguel Castillo <miguel.castillo@ibit.pe>
2023-09-24 22:47:24 -0500

    feat: agregando funcionalidad de agregar usuarios

--- main.py
+++ main.py
@@ -21,20 +21,47 @@ def listarUsuarios():
     for row in cursor.fetchall():
         print(row)
 
+def insertarUsuario(nombre, correo):
+    try: 
+        query = "INSERT INTO usuarios (nombre, correo) values(%s, %s)"
+        datos = (nombre, correo)
+
+        cursor.execute(query, datos)
+        connection.commit()
+        print("Usuario insertado!")
+    except mysql.connector.Error as error:
+        print(f"Error al crear el usuario: {error}")
+        connection.rollback()
+
 def main():
-    print("Bienvenido al cliente de python para manejar una base de datos")
-    print("\nOpciones:")
-    print("1. Listar registros")
-    print("2. Insertar registro")
-    print("3. Actualizar registro")
-    print("4. Eliminar registro")
-    print("5. Salir")
+    salir = False
+    while(salir == False):
+        print("Bienvenido al cliente de python para manejar una base de datos")
+        print("\nOpciones:")
+        print("1. Listar registros")
+        print("2. Insertar registro")
+        print("3. Actualizar registro")
+        print("4. Eliminar registro")
+        print("5. Limpiar consola")
+        print("6. Salir")
 
-    choice = input("Seleccione una opción: ")
+        choice = input("Seleccione una opción: ")
 
-    if choice == "1": 
-        print("Listar usuarios:")
-        listarUsuarios()
+        if choice == "1": 
+            print("Listar usuarios:")
+            listarUsuarios()
+        if choice == "2":
+            print("Se necesitan el nombre y correo del usuario: ")
+            nombre = input("Por favor ingrese el nombre del usuario: ")
+            correo = input("Por favor ingrese el correo del usuario: ")
+
+            insertarUsuario(nombre, correo)
+        if choice == "5":
+            os.system('cls')
+        if choice == "6":
+            print("Hasta luego!")
+            salir = True
+
 
 
 if __name__ == "__main__":
